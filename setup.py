76823a20947959ab1f6d4c097e5eaa1a2b1b0350
boris <boris@verhovs.ky>
2019-08-11 15:06:33 -0600

    bumped version from 7.1->9.0 and other setup.py changes

--- setup.py
+++ setup.py
@@ -2,25 +2,34 @@
 
 from distutils.core import setup
 
-setup(name="dtrx",
-      version = "7.1",
-      description = "Script to intelligently extract multiple archive types",
-      author = "Brett Smith",
-      author_email = "brettcsmith@brettcsmith.org",
-      url = "http://www.brettcsmith.org/2007/dtrx/",
-      download_url = "http://www.brettcsmith.org/2007/dtrx/",
-      scripts = ['scripts/dtrx'],
-      license = "GNU General Public License, version 3 or later",
-      classifiers = ['Development Status :: 5 - Production/Stable',
-                     'Environment :: Console',
-                     'Intended Audience :: End Users/Desktop',
-                     'Intended Audience :: System Administrators',
-                     'License :: OSI Approved :: GNU General Public License (GPL)',
-                     'Natural Language :: English',
-                     'Operating System :: POSIX',
-                     'Programming Language :: Python',
-                     'Topic :: Utilities'],
-      long_description = """dtrx extracts archives in a number of different
+setup(
+    name="dtrx",
+    version="9.0",
+    description="Script to intelligently extract multiple archive types",
+    author="Brett Smith",
+    author_email="brettcsmith@brettcsmith.org",
+    url="https://github.com/verhovsky/dtrx",
+    download_url="https://github.com/verhovsky/dtrx",
+    scripts=["scripts/dtrx"],
+    license="GNU General Public License, version 3 or later",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Utilities",
+    ],
+    long_description="""dtrx extracts archives in a number of different
       formats; it currently supports tar, zip (including self-extracting
       .exe files), cpio, rpm, deb, gem, 7z, cab, rar, lzh, arj, and
       InstallShield files.  It can also decompress files compressed with gzip,
@@ -31,5 +40,5 @@ setup(name="dtrx",
       By default, everything will be written to a dedicated directory
       that's named after the archive.  dtrx will also change the
       permissions to ensure that the owner can read and write all those
-      files."""
-      )
+      files.""",
+)
